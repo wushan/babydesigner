12674d24f028d3f9458ad246f343b3b61b8ac847
wushan <chiangloveslynn@gmail.com>
2016-07-22 18:23:15 +0800

    upload preview images to S3.

--- api/controllers/WorksController.js
+++ api/controllers/WorksController.js
@@ -8,6 +8,9 @@ var shortid = require('shortid');
 var mkdirp = require('mkdirp');
 var fs = require('fs');
 var getDirName = require('path').dirname;
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath('./s3_config.json');
+var s3Bucket = new AWS.S3( { params: {Bucket: 'draftty'} } );
 
 function writeFile(path, contents, cb) {
   mkdirp(getDirName(path), function (err) {
@@ -39,16 +42,35 @@ module.exports = {
 	},
 	updateWork: function(req,res) {
 		var base64 = req.body.thumbnail;
-		var buf = base64.split(',')[1];
+		// var buf = base64.split(',')[1];
 		sails.log('Work Updated.')
-		// Update the record and write the thumbnail
-		var savePath = "preview/" + req.body.workid + ".png";
-		writeFile(".tmp/public/preview/" + req.body.workid + ".png", buf, 'base64', function(err){
-		  if(err) {
-		    throw err;
-		  }
-		  sails.log('file writed');
-		});
+		// // Update the record and write the thumbnail
+		// var savePath = "preview/" + req.body.workid + ".png";
+		// writeFile(".tmp/public/preview/" + req.body.workid + ".png", buf, 'base64', function(err){
+		//   if(err) {
+		//     throw err;
+		//   }
+		//   sails.log('file writed');
+		// });
+		//Upload this fucker to S3
+		var savePath = "https://" + s3Bucket.config.endpoint + "/" + s3Bucket.config.params.Bucket + '/' + req.body.workid;
+		var buf = new Buffer(base64.replace(/^data:image\/\w+;base64,/, ""),'base64');
+		  var data = {
+		    Key: req.body.workid, 
+		    Body: buf,
+		    ContentEncoding: 'base64',
+		    ContentType: 'image/jpeg'
+		  };
+
+		  s3Bucket.putObject(data, function(err, data){
+		      if (err) { 
+		        console.log(err);
+		        console.log('Error uploading data: ', data); 
+		      } else {
+		      	sails.log(s3Bucket);
+		        console.log('succesfully uploaded the image!');
+		      }
+		  });
 
 		Works.update({author: req.user.id, workid: req.body.workid },{data: req.body.data, thumbnail: savePath, public: req.body.public, category: req.body.category, subcategory: req.body.subcategory, workwidth: req.body.workwidth, workheight: req.body.workheight }).exec(function afterwards(err, updated){
 		  if (err) {
@@ -76,3 +98,15 @@ module.exports = {
 	}
 };
 
+//Global
+function dataURItoBlob(dataURI) {
+  // convert base64/URLEncoded data component to raw binary data held in a string
+  var binary = atob(dataURI.split(',')[1]);
+  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+  var array = [];
+  for(var i = 0; i < binary.length; i++) {
+    array.push(binary.charCodeAt(i));
+  }
+  return new Blob([new Uint8Array(array)], {type: mimeString});
+}
